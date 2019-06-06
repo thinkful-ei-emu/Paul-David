f7040ed089dff872bd825ef7dd63eacdb9957a03
David Queen <dsqjr@msn.com>
2019-06-06 12:33:51 -0400

    index.js edit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 /*eslint-env jquery*/
 const cats = $('.lilcat>img');
-$('lilcat').on('click',cats,(e)=>{
+$('.lilcat').on('click',cats,(e)=>{
   $('.top>img').attr('src',$(e.currentTarget).attr('src'));
   e.preventDefault();
 });
