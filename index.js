82d63700425a6a96b5f09015b8d836d0fa79b862
Paul Han <kyutae.han93@gmail.com>
2019-06-06 13:31:36 -0400

    changed fizz.js into fizzbuzz.js. Fixed cat carousel to work with keyboard

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ function main(){
     $('.top>img').attr('src',$(e.target).attr('src'));
     e.preventDefault();
   });
+  $('.lilcat').keypress(function (e) {
+    var key = e.which;
+    if(key === 13){
+      $('.top>img').attr('src',$(e.target).attr('src'));
+    }
+    e.preventDefault();
+  }); 
 }
 
 $(main);
